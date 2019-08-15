9947bf48594e5adcf2615d89f26554c2835dd511
Bas Bossink <bas.bossink@divverence.com>
2019-08-15 13:32:56 +0200

    Add Nuget_profile

---
+++ Nuget_profile.ps1
@@ -0,0 +1,23 @@
+﻿function Get-PackageVersions {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True,
+                  Position=0)]
+        [string]
+        $packageId,
+
+        [Parameter(Mandatory=$False)]
+        [Switch]
+        $IncludePreRelease=$False
+    )
+
+    Find-Package $packageId `
+      -IncludePreRelease:$IncludePreRelease `
+      -AllVersions `
+      -ExactMatch `
+      -Source "https://www.myget.org/F/divverence/api/v3/index.json" |
+      Select-Object -ExpandProperty Versions |
+      Select-Object -ExpandProperty OriginalVersion
+}
+
+New-Alias -Name gpvs Get-PackageVersions
